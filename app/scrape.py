16cac38783cb89215fb9b7d2af63be3e50889a58
Phi Bui <phibangbui.cs@gmail.com>
2014-12-18 15:38:04 -0800

    webdriver now goes through all links properly, now need to parse table to find course info

--- app/scrape.py
+++ app/scrape.py
@@ -4,30 +4,35 @@ MAIN_URL = 'https://go.sfu.ca/psp/paprd/EMPLOYEE/h/?tab=PAPP_GUEST'
 
 OPEN_IMG_SRC = 'https://sims-prd.sfu.ca/cs/csprd/cache/PS_CS_STATUS_OPEN_ICN_1.gif'
 
-def go_to(course, coursenum, section):
+def go_to(course, coursenum):
 	# switch to frame containing the elements we're looking for
 	browser.switch_to_frame("TargetContent")
 	# Go to course's alphanumeric category
 	browser.find_element_by_id("DERIVED_SSS_BCC_SSR_ALPHANUM_" + course[0]).click()
 	# Expand Course View
+	browser.implicitly_wait(10)
 	browser.find_element_by_partial_link_text(course).click()
 	# Click on course requested
+	browser.implicitly_wait(10)
 	browser.find_element_by_link_text(coursenum).click()
 	# View all class sections
-	browser_find_element_by_id("DERIVED_SAA_CRS_SSR_PB_GO").click()
-	print element.get_attribute("innerHTML")
-	
+	browser.implicitly_wait(10)
+	browser.find_element_by_id("DERIVED_SAA_CRS_SSR_PB_GO").click()
+	print "got to end point\n"
+
+
 browser = webdriver.PhantomJS()
 browser.set_window_size(1400, 1000)
 
 #Go to main page
 browser.get(MAIN_URL)
-print "got onto main page: " + browser.current_url
 
 #Go to course catalog page
 browser.find_element_by_link_text("Browse Course Catalog").click()
-print "clicked on " + browser.current_url
-go_to("ACMA", "210")
+
+#Browser goes on course page with all class sections open
+go_to("CHIN", "100")
+
 browser.quit()
 
 
