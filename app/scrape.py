d534260758ead6ad963c50d1ea5e48d2e8003255
Phi Bui <phibangbui.cs@gmail.com>
2014-12-18 22:46:15 -0800

    parsing fully functional, some refactoring is probably there but... oh well

--- app/scrape.py
+++ app/scrape.py
@@ -5,6 +5,14 @@ MAIN_URL = 'https://go.sfu.ca/psp/paprd/EMPLOYEE/h/?tab=PAPP_GUEST'
 OPEN_IMG_SRC = 'https://sims-prd.sfu.ca/cs/csprd/cache/PS_CS_STATUS_OPEN_ICN_1.gif'
 
 def check_course(course, coursenum, section):
+	browser = webdriver.PhantomJS()
+	browser.set_window_size(1400, 1000)
+
+	# Go to main page
+	browser.get(MAIN_URL)
+	# Go to course catalog page
+	browser.implicitly_wait(10)
+	browser.find_element_by_link_text("Browse Course Catalog").click()
 	# switch to frame containing the elements we're looking for
 	browser.switch_to_frame("TargetContent")
 	# Go to course's alphanumeric category
@@ -18,34 +26,28 @@ def check_course(course, coursenum, section):
 	# View all class sections
 	browser.implicitly_wait(10)
 	browser.find_element_by_id("DERIVED_SAA_CRS_SSR_PB_GO").click()
-	# Check if course section status image is OPEN_IMG_SRC
-	# Iterate through all sections until we get the element that has what we want
-	browser.implicitly_wait(10)
 	# Get the table row for the section that we're looking for
-	element = browser.find_element_by_xpath("//tr[contains(text()," + section + ")]")
+	browser.implicitly_wait(10)
+	browser.find_element_by_partial_link_text(section).click()
 
-	# Check if the image element within the row is equal to the OPEN_IMG_SRC
-	# ...there's probably a better way of doing this
-	if (element.find_elements_by_xpath(".//td[@src=" + OPEN_IMG_SRC + "]").isEmpty()):
-		print "course is full\n" 
-		return false
-	else:
+	browser.implicitly_wait(10)
+	div_element = browser.find_element_by_id("win0divSSR_CLS_DTL_WRK_SSR_STATUS_LONG")
+	img_element = div_element.find_element_by_class_name("SSSIMAGECENTER")
+	element_src = img_element.get_attribute('src')
+	if (element_src == OPEN_IMG_SRC):
 		print "course has seats\n"
-		return true
+		browser.quit()
+		return True
+	else:
+		print "course is full\n"
+		browser.quit()
+		return False
 
-
-browser = webdriver.PhantomJS()
-browser.set_window_size(1400, 1000)
-
-#Go to main page
-browser.get(MAIN_URL)
-
-#Go to course catalog page
-browser.find_element_by_link_text("Browse Course Catalog").click()
 
 #Browser goes on course page with all class sections open
-check_course("CHIN", "100", "D400-TUT")
+print "should print has seats \n"
+check_course("HIST", "101", "D100-LEC")
 
-browser.quit()
-
+print "should print is full \n"
+check_course("HIST", "101", "D101-TUT")
 
