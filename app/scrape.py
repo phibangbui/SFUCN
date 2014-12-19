eaa16fee500a4558485fc7a3afd541b3fd9c23be
Phi Bui <phibangbui.cs@gmail.com>
2014-12-18 16:56:15 -0800

    obtained table row, all that's left is to parse for the image

--- app/scrape.py
+++ app/scrape.py
@@ -4,7 +4,7 @@ MAIN_URL = 'https://go.sfu.ca/psp/paprd/EMPLOYEE/h/?tab=PAPP_GUEST'
 
 OPEN_IMG_SRC = 'https://sims-prd.sfu.ca/cs/csprd/cache/PS_CS_STATUS_OPEN_ICN_1.gif'
 
-def go_to(course, coursenum):
+def check_course(course, coursenum, section):
 	# switch to frame containing the elements we're looking for
 	browser.switch_to_frame("TargetContent")
 	# Go to course's alphanumeric category
@@ -18,7 +18,20 @@ def go_to(course, coursenum):
 	# View all class sections
 	browser.implicitly_wait(10)
 	browser.find_element_by_id("DERIVED_SAA_CRS_SSR_PB_GO").click()
-	print "got to end point\n"
+	# Check if course section status image is OPEN_IMG_SRC
+	# Iterate through all sections until we get the element that has what we want
+	browser.implicitly_wait(10)
+	# Get the table row for the section that we're looking for
+	element = browser.find_element_by_xpath("//tr[contains(text()," + section + ")]")
+
+	# Check if the image element within the row is equal to the OPEN_IMG_SRC
+	# ...there's probably a better way of doing this
+	if (element.find_elements_by_xpath(".//td[@src=" + OPEN_IMG_SRC + "]").isEmpty()):
+		print "course is full\n" 
+		return false
+	else:
+		print "course has seats\n"
+		return true
 
 
 browser = webdriver.PhantomJS()
@@ -31,7 +44,7 @@ browser.get(MAIN_URL)
 browser.find_element_by_link_text("Browse Course Catalog").click()
 
 #Browser goes on course page with all class sections open
-go_to("CHIN", "100")
+check_course("CHIN", "100", "D400-TUT")
 
 browser.quit()
 
